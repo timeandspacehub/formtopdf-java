d76465d2e44d19b48dccdea29000a42a002068d6
Nijjwal Shrestha <timeandspacehub@gmail.com>
2025-11-21 19:24:19 -0700

    16 - Added AuthController

---
+++ src/main/java/com/timeandspacehub/formtopdf/controller/AuthController.java
@@ -0,0 +1,17 @@
+package com.timeandspacehub.formtopdf.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class AuthController {
+
+    @GetMapping("/auth-check")
+    public ResponseEntity<String> authCheck() {
+        // If we reach here, user is authenticated by Spring Security
+        return ResponseEntity.ok("OK");
+    }
+}
