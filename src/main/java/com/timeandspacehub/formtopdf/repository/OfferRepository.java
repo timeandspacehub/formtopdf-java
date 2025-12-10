14d050fd2f1c297651e9569720171f4df075e488
Kaushal Silwal <silwalkaushal1001@gmail.com>
2025-12-10 18:05:57 -0500

    42- Updated user based view

--- src/main/java/com/timeandspacehub/formtopdf/repository/OfferRepository.java
+++ src/main/java/com/timeandspacehub/formtopdf/repository/OfferRepository.java
@@ -1,5 +1,7 @@
 package com.timeandspacehub.formtopdf.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,8 @@ import com.timeandspacehub.formtopdf.entity.Offer;
 
 @Repository
 public interface OfferRepository extends JpaRepository<Offer, Integer>{
+	
+	List<Offer> findByCreatedByUsername(String username);
+
     
 }
